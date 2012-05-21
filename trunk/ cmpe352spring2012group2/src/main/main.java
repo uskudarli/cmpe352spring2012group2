aa306e293d29d26a5001cc6edc677858910dc7f3
eylultasyurek@gmail.com <eylultasyurek@gmail.com@4d1ed40c-f783-9d2e-d84a-fd3849104dda>
2012-05-21 15:27:32 +0000

    Prime number less than 2 bug is fixed. Efficiency is improved.

--- trunk/ cmpe352spring2012group2/src/main/main.java
+++ trunk/ cmpe352spring2012group2/src/main/main.java
@@ -1,10 +1,13 @@
 package main;
+import java.util.*;
 
 public class main {
 
 	public boolean isPrime(int x)
 	{
-		for (int i=2; i < x; i++)
+	if (x<2)
+	return false;
+		for (int i=2; i < sqrt(x); i++)
 		{
 			if (x%2==0)
 				return false;
